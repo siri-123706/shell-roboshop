ac94c39570aadedbbdbf63832e2f242dbe9399fe
shirishakodari <kodarishirisha419@gmail.com>
2025-09-10 19:37:45 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -10,7 +10,7 @@ DOMAIN_NAME="daws84s.cfd" # replace with your domain
 #for instance in ${INSTANCES[@]}
 for instance in $@ #all argments passed
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-02b7e678afecd2521 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03f077f464f434b47 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ] 
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
